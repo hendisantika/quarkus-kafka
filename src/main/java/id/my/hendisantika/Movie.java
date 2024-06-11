1cea341b9f1ed58c99d51a29f93180ee3df5e0f9
hendisantika <hendisantika@yahoo.co.id>
2024-06-12 06:53:59 +0700

    Add Movie Model🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/Movie.java
@@ -0,0 +1,16 @@
+package id.my.hendisantika;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : quarkus-kafka
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 6/12/24
+ * Time: 06:53
+ * To change this template use File | Settings | File Templates.
+ */
+public class Movie {
+    public String title;
+    public int year;
+}
